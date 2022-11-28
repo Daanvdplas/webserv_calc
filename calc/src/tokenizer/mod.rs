044d0cbed2f6a79d66480ec6edffbbfd3dda4863
Daan van der Plas <daanvdplas@live.nl>
2022-11-28 18:53:14 +0100

    tokenizer raw version, started with tests

--- calc/src/tokenizer/mod.rs
+++ calc/src/tokenizer/mod.rs
@@ -12,12 +12,12 @@ pub enum Operator {
 #[derive(PartialEq, Debug)]
 pub enum Token {
     Begin,
-    Minus,
+    Minus(u8),
     Operator(Operator),
     Number(i64),
 }
 
-pub fn tokenizer(input: String) -> Result<Vec<Token>, &'static str> {
+pub fn tokenizer(input: String) -> Result<Vec<Token>, String> {
     let mut tokens: Vec<Token> = Vec::new();
     let mut token: Token = Token::Begin;
     let mut bracket_count: u8 = 0;
@@ -25,7 +25,7 @@ pub fn tokenizer(input: String) -> Result<Vec<Token>, &'static str> {
     for x in input.chars() {
         // Check if character is allowed
         if !"0123456789-+/*^ ()".contains(x) {
-            return Err("Wrong input; only digits and operators (+ - * / ^ ( ))");
+            return Err("Wrong input; only digits and operators (+ - * / ^ ( ))".to_string());
         }
 
         if x == ' ' { continue }
@@ -34,10 +34,13 @@ pub fn tokenizer(input: String) -> Result<Vec<Token>, &'static str> {
         if "0123456789".contains(x) {
             match token {
                 Token::Begin => token = Token::Number(x as i64 - 48),
-                Token::Minus => token = Token::Number(-1 * (x as i64 - 48)),
+                Token::Minus(amount) => {
+                    if amount % 2 == 0 { token = Token::Number(x as i64 - 48); }
+                    else { token = Token::Number(-1 * (x as i64 - 48)); }
+                },
                 Token::Number(nbr) => token = Token::Number(nbr * 10 + (x as i64 - 48)),
                 Token::Operator(ref operator) => {
-                    if operator == &Operator::ClosingBracket { return Err("Immediate nbr after closing bracket"); }
+                    if operator == &Operator::ClosingBracket { return Err("Immediate nbr after closing bracket".to_string()); }
                     else { 
                         tokens.push(token);
                         token = Token::Number(x as i64 - 48);
@@ -52,12 +55,15 @@ pub fn tokenizer(input: String) -> Result<Vec<Token>, &'static str> {
             match token {
                 Token::Begin => { 
                     match x {
-                        '-' => token = Token::Minus,
+                        '-' => token = Token::Minus(1),
                         '(' => token = Token::Operator(Operator::OpenBracket),
-                        _ => return Err("Error: 5"),
+                        _ => return Err("Error: 5".to_string()),
                     };
                 },
-                Token::Minus => return Err("Error: minus"),
+                Token::Minus(amount) => {
+                    if x == '-' { token = Token::Minus(amount + 1); }
+                    else { return Err("Error: minus".to_string()); }
+                },
                 Token::Number(nbr) => {
                     tokens.push(token);
                     token = make_operator_token(x);
@@ -66,29 +72,33 @@ pub fn tokenizer(input: String) -> Result<Vec<Token>, &'static str> {
                     Operator::Substract | Operator::Add | Operator::Multiply | Operator::Divide | Operator::Square => {
                         if x == '-' { 
                             tokens.push(token);
-                            token = Token::Minus;
-                        } else { return Err("Error: double operator 1"); }
+                            token = Token::Minus(1);
+                        } else if x == '(' {
+                            tokens.push(token);
+                            token = Token::Operator(Operator::OpenBracket);
+                        } else { return Err(format!("Error: double operator 1, with token: {token:?}")); }
                     },
                     Operator::OpenBracket => {
-                        if x == '(' {
+                        if dbg!(x) == '(' {
                             tokens.push(token);
                             token = Token::Operator(Operator::OpenBracket);
                             bracket_count += 1;
-                        } else { return Err("Error: double operator 2"); }
+                        } else { return Err("Error: double operator 2".to_string()); }
                     },
                     Operator::ClosingBracket => {
                         if x == ')' {
-                            if !bracket_check(x, &mut bracket_count) { return Err("Error: bracket"); }
-                            tokens.push(token);
-                            token = Token::Operator(Operator::ClosingBracket);
-                        } else { return Err("Error: double operator 3"); }
+                            if !bracket_check(x, &mut bracket_count) { return Err("Error: bracket".to_string()); }
+                        } 
+                        tokens.push(token);
+                        token = make_operator_token(x);
                     },
                 },
             };
         }
     }
+    tokens.push(token);
     if bracket_count != 0 {
-        return Err("Error: bracket count");
+        return Err("Error: bracket count".to_string());
     }
     println!("{tokens:?}");
     Ok(tokens)
@@ -119,4 +129,30 @@ fn bracket_check(x: char, bracket_count: &mut u8) -> bool {
     true
 }
 
-#cfg[test
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn simple_tests() {
+        assert_eq!(tokenizer(String::from("234 + 4 ^ 3 / 2")), 
+                   Ok(vec![Token::Number(234), Token::Operator(Operator::Add), Token::Number(4), Token::Operator(Operator::Square),
+                    Token::Number(3), Token::Operator(Operator::Divide), Token::Number(2)]));
+
+        assert_eq!(tokenizer(String::from("234+4^3/2")), 
+                   Ok(vec![Token::Number(234), Token::Operator(Operator::Add), Token::Number(4), Token::Operator(Operator::Square),
+                    Token::Number(3), Token::Operator(Operator::Divide), Token::Number(2)]));
+
+        assert_eq!(tokenizer(String::from("234+-4^3/2")), 
+                   Ok(vec![Token::Number(234), Token::Operator(Operator::Add), Token::Number(-4), Token::Operator(Operator::Square),
+                    Token::Number(3), Token::Operator(Operator::Divide), Token::Number(2)]));
+    }
+
+    #[test]
+    fn bracket_tests() {
+        assert_eq!(tokenizer(String::from("234 + (323- 1 * 4) / 2")), 
+                   Ok(vec![Token::Number(234), Token::Operator(Operator::Add), Token::Operator(Operator::OpenBracket),
+                    Token::Number(323), Token::Operator(Operator::Substract), Token::Number(1), Token::Operator(Operator::Multiply),
+                    Token::Number(4), Token::Operator(Operator::ClosingBracket), Token::Operator(Operator::Divide), Token::Number(2)]));
+    }
+}
---
+++ calc/target/debug/deps/calc-6f30709eae90d128.d
@@ -0,0 +1,6 @@
+/Users/parity/Documents/webserv_calc/calc/target/debug/deps/calc-6f30709eae90d128: src/main.rs src/tokenizer/mod.rs
+
+/Users/parity/Documents/webserv_calc/calc/target/debug/deps/calc-6f30709eae90d128.d: src/main.rs src/tokenizer/mod.rs
+
+src/main.rs:
+src/tokenizer/mod.rs:
